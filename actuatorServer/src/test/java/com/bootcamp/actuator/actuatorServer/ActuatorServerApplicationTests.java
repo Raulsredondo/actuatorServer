5b80884b2eac5a6c7121d61cfaf5cb780c21bf4a
Raulsredondo <raulsredondo2@gmail.com>
2021-12-22 17:28:36 +0100

    Initial commit

---
+++ actuatorServer/src/test/java/com/bootcamp/actuator/actuatorServer/ActuatorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.actuator.actuatorServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
